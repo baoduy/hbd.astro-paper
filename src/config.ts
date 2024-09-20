edad466d1513e737a10df9097baad6e41e828df8
Steven Hoang <drunkcoding@outlook.com>
2024-09-20 14:50:33 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "drunkcoding.net",
   //ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 5,
+  postPerPage: 10,
   postPerIndex: 5,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
